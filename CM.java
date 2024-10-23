4f8fe78addee80f0ad1227e5b8096bfda9db1202
Hector Prado <Pradohm@gmail.com>
2024-10-22 22:49:08 -0300

    Trabalho calculo medio EBAC

---
+++ CM.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CM {
+
+    public static void CalcularMedia(int a, int b, int c, int d) {
+        double somaTotal = (a + b + c + d);
+        double media = (somaTotal / 4);
+        System.out.println("A média dos 4 valores é:" + media);
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CM Sala1 = new CM();
+
+        Sala1.CalcularMedia(4,3,5,6);
+
+
+    }
+}
